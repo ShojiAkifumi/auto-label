750d4c3ffd28d3aa86cec17c2a5641bf874df325
aki <60290657+ShojiAkifumi@users.noreply.github.com>
2022-03-31 07:36:06 +0900

    use select lotnum

--- app.py
+++ app.py
@@ -42,12 +42,16 @@ def main():
         size1 = st.number_input("サイズ(縦幅)",step=0.1,value=1.0)
     with col2:
         size2 = st.number_input("サイズ(横幅)",step=0.1,value=1.0)
-
-    if st.checkbox('新規ロット番号'):
+    
+    flg_lot = False
+    
+    if flg_lot:
         lot_num = st.text_input("ロット番号", placeholder='T-442251-1-3')
         if hankaku(lot_num): st.error('ロット番号は半角で入力してください')
     else:
         st.selectbox('ロット番号',('T-442251-1-3','T-642584-2-3'))
+
+    flg_lot = st.checkbox('新規ロット番号')
 
     col1, col2 = st.columns(2)
     with col1:
